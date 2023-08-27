0d226e860113c31929dc526aa2074d9fe42504d9
danny8823 <dannyyoo714@gmail.com>
2023-08-26 20:56:04 -0700

    added cors configuration

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ import dotenv from 'dotenv'
 import { db } from './db/db.js'
 import routers from './routes/index.js'
 import { seed } from './seed.js'
+import cors from 'cors'
 
 const port = process.env.PORT
 const server = express()
+const allowedOrigins = ['https://connectblog-com.preview-domain.com'];
 
+server.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 server.use('/', routers) 
